029b02c0168a53b928e6d06c8ad716f6b54ccb64
Mesiahs <sebastianmesias04@gmail.com>
2026-02-03 19:43:32 -0500

    Primer cambio

---
+++ src/routes/user.routes.js
@@ -0,0 +1,27 @@
+import {Router} from 'express'
+
+const router = Router();
+
+router.get('/users', (req, res) => {
+    res.send('obteniendo usuarios')
+})
+
+router.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    res.send('obteniendo usuario' )
+})
+
+router.post('/users', (req, res) => {
+    res.send('creando usuario')
+})
+
+router.get('/users', (req, res) => {
+    res.send('obteniendo usuarios')
+})
+
+router.get('/users', (req, res) => {
+    res.send('obteniendo usuarios')
+})
+
+
+export default router; 
